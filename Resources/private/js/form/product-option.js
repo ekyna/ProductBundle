3a02deb974a21faddde41182d3aad51211eb584b
Etienne Dauvergne <contact@ekyna.com>
2017-10-31 17:56:07 +0100

    [Product] Fix option form js. Show and form tabs identifiers.

--- Resources/private/js/form/product-option.js
+++ Resources/private/js/form/product-option.js
@@ -17,10 +17,6 @@ define(['jquery'], function($) {
 
                 $form.find('.option-wrapper').hide();
                 $form.find('.option-' + mode).show();
-
-                if (mode === 'data') {
-                    $product.val(undefined).find('option:selected').prop('checked', false);
-                }
             }
 
             $mode.on('change', onChange);
@@ -34,6 +30,18 @@ define(['jquery'], function($) {
     return {
         init: function($element) {
             $element.optionWidget();
+        },
+        save: function($element) {
+            $element.each(function() {
+                var $form = $(this),
+                    $mode = $form.find('div.option-mode input[type="radio"]'),
+                    $product = $form.find('select.entity-search');
+
+                var mode = $mode.filter(':checked').val();
+                if (mode === 'data') {
+                    $product.val(undefined).find('option:selected').prop('selected', false);
+                }
+            });
         }
     };
 });
