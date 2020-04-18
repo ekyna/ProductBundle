814a872866a564a3a67598a13419290ef0a0d8a0
Etienne Dauvergne <contact@ekyna.com>
2020-04-18 17:27:18 +0200

    [Product] Fixed product reference table filter.

--- Table/Filter/ProductReferenceType.php
+++ Table/Filter/ProductReferenceType.php
@@ -3,6 +3,7 @@
 namespace Ekyna\Bundle\ProductBundle\Table\Filter;
 
 use Ekyna\Component\Table\Bridge\Doctrine\ORM\Source\EntityAdapter;
+use Ekyna\Component\Table\Bridge\Doctrine\ORM\Util\FilterUtil;
 use Ekyna\Component\Table\Context\ActiveFilter;
 use Ekyna\Component\Table\Extension\Core\Type\Filter\TextType;
 use Ekyna\Component\Table\Filter\AbstractFilterType;
@@ -28,14 +29,18 @@ class ProductReferenceType extends AbstractFilterType
         $qb = $adapter->getQueryBuilder();
         $alias = $qb->getRootAliases()[0];
 
+        $parameter = FilterUtil::buildParameterName('reference');
+        $operator = $activeFilter->getOperator();
+        $value = FilterUtil::buildParameterValue($operator, $activeFilter->getValue());
+
         $vReferencePath = $adapter->getQueryBuilderPath('variants.reference');
 
         $qb
             ->andWhere($qb->expr()->orX(
-                $qb->expr()->eq($alias . '.reference', ':reference'),
-                $qb->expr()->eq($vReferencePath, ':reference')
+                FilterUtil::buildExpression($alias . '.reference', $operator, $parameter),
+                FilterUtil::buildExpression($vReferencePath, $operator, $parameter)
             ))
-            ->setParameter('reference', $activeFilter->getValue());
+            ->setParameter($parameter, $value);
 
         return true;
     }
